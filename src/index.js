5bf7c1137571fd1ca05f865b42365670cb926ee9
Paul Landaeta <paul.landeata@coxautoinc.com>
2022-09-06 22:22:36 -0400

    set port

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ const express = require('express');
 const routes = require('./routes');
 const app = express();
 const cors = require('cors');
+app.set('port', (process.env.S_PORT || 5000));
 
 app.use(cors());
-
 app.use(express.json());
 app.use('/api', routes);
 
-app.listen(process.env.S_PORT || 5000, () => {
-    console.log('Happy Codding the app listening on port 5000!');
+app.listen(app.get('port'), () => {
+    console.log('Happy Codding the app listening on port', process.env.S_PORT);
 });
